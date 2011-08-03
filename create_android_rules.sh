b417c10bc7c9e1fa0bd1cbe370d5578673fe65ae
LinuxMotion <jweyrauch359@gmail.com>
2011-08-03 15:26:47 -0700

    Add a check to see if ADB exits. Kill the adb server only if present

--- create_android_rules.sh
+++ create_android_rules.sh
@@ -1,6 +1,39 @@
 #!/bin/bash
 
 
+
+exists()
+{
+        if command -v $1 &>/dev/null
+        then
+               # echo " Yes, command :$1: was found."
+                return 0
+        else
+                # echo " No, command :$1: was NOT found."
+                return 1
+        fi
+}
+
+killadbserver(){
+
+	echo "Adb found, killing the adb server"
+	adb kill-server
+
+	echo
+	echo "*******************************************************"
+	echo "Please unplug and replug any currently connected devices"
+	echo "Press enter to continue"
+	read pause
+
+	echo ""
+	echo "*******************************************************"
+
+	adb devices
+
+
+}
+
+
 echo ""
 echo "*******************************************************"
 echo "Add device permisions to the udev rules by LinuxMotion"
@@ -12,24 +45,23 @@ then
 echo "Restarting udev to read the new rules"
 sudo service udev restart
 
-echo 
-echo "Killing the adb server"
-
-adb kill-server
-
-echo
-echo "*******************************************************"
-echo "Please unplug and replug any currently connected devices"
-echo "Press enter to continue"
-read pause
-
-echo ""
-echo "*******************************************************"
-
-adb devices
 
 else
 
 echo "Your files was not created, please run again with sudo"
+
 fi
+
+adbexists=&(exists adb)
+echo $x
+
+	if [ $adbexists-eq0 ] 
+	then
+
+	killadbserver
+	
+	else	
+	echo "ADB is not in your path, but the script was created"
+	fi
+
 
